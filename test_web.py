ac83ae26033697948a38e9a2266627b04c87bd9f
mertljan <jm.ostrava@gmail.com>
2021-09-04 07:39:13 +0200

    Initial commit

---
+++ test_web.py
@@ -0,0 +1,14 @@
+import requests
+import bs4
+
+response = requests.get('https://httpbin.org')
+print(response.status_code)
+# print(response.text)
+print(response.json)
+
+response1 = requests.post('http://httpbin.org/post', data = {'my_message':'Hello'})
+print(response1.json)
+
+getr = requests.get('http://example.com/')
+print(getr.text)
+soup = bs4.BeautifulSoup(getr.text, "html.parser")
